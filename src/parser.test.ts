cc8a4d7265c49a2820f8f8e321cb83b55b0d934d
Masaki Hara <ackie.h.gmai@gmail.com>
2022-02-13 16:37:13 +0900

    Initial parser implementation

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,5 +1,42 @@
 import { describe, expect, it, xit } from "@jest/globals";
-import { tokenize } from "./parser";
+import { Expression, parse, tokenize } from "./parser";
+
+describe("parse", () => {
+  it("parses binary 1 + 1", () => {
+    const expected: Expression = {
+      type: "AddExpression",
+      lhs: { type: "NumberExpression", value: 1 },
+      rhs: { type: "NumberExpression", value: 1 },
+    };
+    expect(parse("1 + 1")).toEqual(expected);
+  });
+
+  it("parses ternary 1 + 2 + 3", () => {
+    const expected: Expression = {
+      type: "AddExpression",
+      lhs: {
+        type: "AddExpression",
+        lhs: { type: "NumberExpression", value: 1 },
+        rhs: { type: "NumberExpression", value: 2 },
+      },
+      rhs: { type: "NumberExpression", value: 3 },
+    };
+    expect(parse("1 + 2 + 3")).toEqual(expected);
+  });
+
+  xit("parses parentheses", () => {
+    const expected: Expression = {
+      type: "AddExpression",
+      lhs: { type: "NumberExpression", value: 1 },
+      rhs: {
+        type: "AddExpression",
+        lhs: { type: "NumberExpression", value: 2 },
+        rhs: { type: "NumberExpression", value: 3 },
+      },
+    };
+    expect(tokenize("1 + (2 + 3)")).toEqual(expected);
+  });
+});
 
 describe("tokenize", () => {
   it("tokenizes a text", () => {
--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,61 @@
+
+export type Expression = AddExpression | NumberExpression;
+export type AddExpression = {
+  type: "AddExpression",
+  lhs: Expression,
+  rhs: Expression,
+}
+export type NumberExpression = {
+  type: "NumberExpression",
+  value: number,
+}
+
+export class ParseError extends Error {
+  constructor(message: string) {
+    super(message);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+
+    this.name = this.constructor.name;
+  }
+}
+
+export function parse(text: string): Expression {
+  const tokens = tokenize(text);
+  return new Parser(tokens).parseExpression();
+}
+
+class Parser {
+  private pos = 0;
+  constructor(public readonly tokens: readonly string[]) {}
+  public parsePrimaryExpression(): Expression {
+    if (this.pos >= this.tokens.length) {
+      throw new ParseError("Unexpected EOF");
+    }
+    if (/\d+/.test(this.tokens[this.pos])) {
+      return { type: "NumberExpression", value: parseInt(this.tokens[this.pos++]) };
+    }
+  }
+  public parseExpression(): Expression {
+    if (this.pos >= this.tokens.length) {
+      throw new ParseError("Unexpected EOF");
+    }
+    let expr = this.parsePrimaryExpression();
+    while (this.pos < this.tokens.length) {
+      const token = this.tokens[this.pos];
+      if (token === "+") {
+        this.pos++;
+        expr = { type: "AddExpression", lhs: expr, rhs: this.parsePrimaryExpression() };
+      } else {
+        break;
+      }
+    }
+    return expr;
+  }
+}
+
 export function tokenize(text: string): string[] {
   const tokens: string[] = [];
   let i = 0;
