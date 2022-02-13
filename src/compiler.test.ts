e10a13d20c2200ff8d62b0390f4da90a7f853e1b
Masaki Hara <ackie.h.gmai@gmail.com>
2022-02-13 15:53:00 +0900

    Load test cases from files

--- src/compiler.test.ts
+++ src/compiler.test.ts
@@ -1,4 +1,6 @@
-import { describe, expect, it, xit } from "@jest/globals";
+import path from "path";
+import fs from "fs";
+import { describe, expect, it, it as realIt, xit } from "@jest/globals";
 import { compile } from "./compiler";
 
 describe("compile", () => {
@@ -8,5 +10,20 @@ describe("compile", () => {
 
   xit("Compiles a text to JS", () => {
     expect(compile("1 + 1")).toBe("1n + 1n");
+  });
+
+  describe("testcases", () => {
+    const testcasesDir = path.resolve(__dirname, "__testcases__");
+    const testcaseNames = fs.readdirSync(testcasesDir);
+    for (const testcaseName of testcaseNames) {
+      const testcaseDir = path.resolve(testcasesDir, testcaseName);
+      const config = JSON.parse(fs.readFileSync(path.resolve(testcaseDir, "config.json"), "utf8"));
+      const input = fs.readFileSync(path.resolve(testcaseDir, "input.umo"), "utf8");
+      const expected = fs.readFileSync(path.resolve(testcaseDir, "output.js"), "utf8");
+      const it = config.pending ? xit : realIt;
+      it(testcaseName, () => {
+        expect(compile(input)).toBe(expected);
+      });
+    }
   });
 });
