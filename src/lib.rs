793408e98cd027dab7ed5442e1e6e4271641c0ee
Masaki Hara <ackie.h.gmai@gmail.com>
2023-03-15 22:02:55 +0900

    Richer values

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,49 @@
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Expr {
-    Lit(i32),
+pub enum Value {
+    Int(i32),
+    Arr(Vec<Value>),
 }
 
-pub fn eval(e: &Expr) -> i32 {
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Expr {
+    Int(i32),
+    Arr(Vec<Expr>),
+}
+
+pub mod expr {
+    use super::*;
+    pub fn int(x: i32) -> Expr {
+        Expr::Int(x)
+    }
+    pub fn arr(a: &[Expr]) -> Expr {
+        Expr::Arr(Vec::from(a))
+    }
+}
+
+pub fn eval(e: &Expr) -> Value {
     match e {
-        Expr::Lit(x) => *x,
+        Expr::Int(x) => Value::Int(*x),
+        Expr::Arr(a) => Value::Arr(a.iter().map(|elem| eval(elem)).collect::<Vec<_>>()),
     }
 }
 
 #[test]
 fn test_lit() {
-    assert_eq!(eval(&Expr::Lit(42)), 42);
+    assert_eq!(eval(&expr::int(42)), Value::Int(42));
+    assert_eq!(
+        eval(&expr::arr(&[
+            expr::int(71),
+            expr::int(101),
+            expr::int(108),
+            expr::int(108),
+            expr::int(111),
+        ])),
+        Value::Arr(vec![
+            Value::Int(71),
+            Value::Int(101),
+            Value::Int(108),
+            Value::Int(108),
+            Value::Int(111),
+        ])
+    );
 }
