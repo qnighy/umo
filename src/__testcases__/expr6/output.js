7b5541aeca89def02422f003b7a268aa29582039
Masaki Hara <ackie.h.gmai@gmail.com>
2023-03-15 11:46:59 +0900

    Restart from scratch

--- src/__testcases__/expr6/output.js
+++
@@ -1,4 +0,0 @@
-const f = (x) => x + 10n;
-assert_eq(f(f(3n)), 23n);
-const compose = (f, g) => (x) => f(g(x));
-assert_eq(compose((x) => x + 10n, (x) => x + 20n)(3n), 33n);
--- src/__testcases__/stmt3/output.js
+++
@@ -1,4 +0,0 @@
-const foo = 1n;
-const foo = 1n;
-const bar = 1;
-const bar = 1;
