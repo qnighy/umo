a6cfe0ec3aa88d36635da4b70247c1428d6a4217
Masaki Hara <ackie.h.gmai@gmail.com>
2023-03-16 21:03:47 +0900

    Evaluate as text

--- src/lib.rs
+++ src/lib.rs
@@ -27,23 +27,58 @@ pub fn eval(e: &Expr) -> Value {
     }
 }
 
+pub fn exec(e: &Expr) -> String {
+    let result = eval(e);
+    let Value::Arr(result) = result else {
+        panic!("Not a string: {:?}", result)
+    };
+    let result = result
+        .iter()
+        .map(|elem| {
+            let Value::Int(elem) = elem else {
+                return None;
+            };
+            if !(0..=255).contains(elem) {
+                return None;
+            }
+            Some(*elem as u8)
+        })
+        .collect::<Option<Vec<_>>>()
+        .unwrap_or_else(|| panic!("Not a string: {:?}", result));
+    String::from_utf8_lossy(&result).into_owned()
+}
+
 #[test]
 fn test_lit() {
     assert_eq!(eval(&expr::int(42)), Value::Int(42));
     assert_eq!(
         eval(&expr::arr(&[
-            expr::int(71),
+            expr::int(72),
             expr::int(101),
             expr::int(108),
             expr::int(108),
             expr::int(111),
         ])),
         Value::Arr(vec![
-            Value::Int(71),
+            Value::Int(72),
             Value::Int(101),
             Value::Int(108),
             Value::Int(108),
             Value::Int(111),
         ])
+    );
+}
+
+#[test]
+fn test_exec_lit() {
+    assert_eq!(
+        exec(&expr::arr(&[
+            expr::int(72),
+            expr::int(101),
+            expr::int(108),
+            expr::int(108),
+            expr::int(111),
+        ])),
+        "Hello"
     );
 }
