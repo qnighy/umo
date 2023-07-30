41d944c62c722c2129496014249a4cbeb8a2feae
Masaki Hara <ackie.h.gmai@gmail.com>
2023-07-30 21:47:15 +0900

    Add mockup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    source: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let source = fs::read_to_string(&args.source).unwrap();
+    if source == "use lang::\"0.0.1\";\nputs(\"Hello, world!\");\n" {
+        println!("Hello, world!");
+    } else {
+        todo!("Proper parsing and execution");
+    }
 }
